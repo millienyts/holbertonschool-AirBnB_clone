df3d4f89b0b2a6c9517bb4cf97a0cead4783471e
Felipe Santos <felipecassiors@gmail.com>
2024-02-22 12:12:06 -0500

    place.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,48 +4,34 @@ from datetime import datetime
 from models import storage
 
 class BaseModel:
-    """
-    Defines all common attributes/methods for other classes.
-    """
-
+    """Base class for all models."""
+    
     def __init__(self, *args, **kwargs):
-        """
-        Initializes a new instance of BaseModel.
-        """
+        """Initialization of the base model."""
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
-                if key != '__class__':
+                if key in ["created_at", "updated_at"]:
+                    value = datetime.fromisoformat(value)
+                if key != "__class__":
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
             storage.new(self)
-
+    
     def __str__(self):
-        """
-        Returns a string representation of the BaseModel instance.
-        """
+        """String representation of the BaseModel class."""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-
+    
     def save(self):
-        """
-        Updates the instance's updated_at attribute with the current datetime
-        and saves the instance to the file storage.
-        """
+        """Updates `updated_at` with the current datetime."""
         self.updated_at = datetime.now()
         storage.save()
-
+    
     def to_dict(self):
-        """
-        Returns a dictionary containing all keys/values of the instance's __dict__.
-        Includes the class name under a key '__class__'.
-        Converts datetime objects to ISO format strings.
-        """
-        model_dict = self.__dict__.copy()
+        """Converts instance into a dictionary format."""
+        model_dict = dict(self.__dict__)
         model_dict['__class__'] = self.__class__.__name__
-        model_dict['created_at'] = model_dict['created_at'].isoformat()
-        model_dict['updated_at'] = model_dict['updated_at'].isoformat()
+        model_dict['created_at'] = self.created_at.isoformat()
+        model_dict['updated_at'] = self.updated_at.isoformat()
         return model_dict
-
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,38 +1,21 @@
 #!/usr/bin/python3
 import json
-from models.base_model import BaseModel
 
 class FileStorage:
-    """Serializes instances to a JSON file & deserializes back to instances."""
+    """Handles long-term storage of all class instances."""
     __file_path = "file.json"
     __objects = {}
-
+    
     def all(self):
-        """Returns the dictionary __objects."""
+        """Returns the dictionary `__objects`."""
         return FileStorage.__objects
-
+    
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id."""
+        """Adds new object to the storage dictionary."""
         if obj:
             key = f"{obj.__class__.__name__}.{obj.id}"
-            FileStorage.__objects[key] = obj
-
+            self.__objects[key] = obj
+    
     def save(self):
-        """Serializes __objects to the JSON file."""
-        obj_dict = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
-        with open(FileStorage.__file_path, 'w') as f:
-            json.dump(obj_dict, f)
-
-    def reload(self):
-        """Deserializes the JSON file to __objects."""
-        try:
-            with open(FileStorage.__file_path, 'r') as f:
-                objs = json.load(f)
-            for k, v in objs.items():
-                class_name = v['__class__']
-                del v['__class__']
-                if class_name == 'BaseModel':
-                    obj = BaseModel(**v)
-                FileStorage.__objects[k] = obj
-        except FileNotFoundError:
-            pass
+        """Serializes `__objects` to the JSON file."""
+        with open(self.__
