b4d403a4253bca7bbdab5e2e636fcb1f1b62a30f
Felipe Santos <felipecassiors@gmail.com>
2024-02-22 17:58:45 -0500

    console.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,36 +1,42 @@
 import json
 from models.base_model import BaseModel
-from models.user import User  # Import additional classes here
+from models.user import User  # Import additional classes as needed
 
 class FileStorage:
-    __file_path = "file.json"
-    __objects = {}
+    __file_path = "file.json"  # Path to the JSON file for storing objects
+    __objects = {}  # Dictionary to store all objects by <class name>.id
+
+    # A dictionary mapping class names to classes, used for deserialization
+    class_dict = {
+        "BaseModel": BaseModel,
+        "User": User,
+        # Add other classes to this dictionary as you create them
+    }
 
     def all(self):
-        """Returns the dictionary __objects"""
+        """Returns the dictionary __objects."""
         return self.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id"""
+        """Adds an object to the __objects dictionary."""
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (path: __file_path)."""
+        """Serializes __objects to the JSON file (__file_path)."""
         obj_dict = {obj_id: obj.to_dict() for obj_id, obj in self.__objects.items()}
         with open(self.__file_path, 'w') as f:
             json.dump(obj_dict, f)
 
     def reload(self):
-        """Deserializes the JSON file to __objects."""
+        """Deserializes the JSON file (__file_path) to __objects if it exists."""
         try:
             with open(self.__file_path, 'r') as f:
                 obj_dict = json.load(f)
             for obj_id, obj_data in obj_dict.items():
                 cls_name = obj_data['__class__']
-                cls = eval(cls_name) if cls_name in self.class_dict else None
-                if cls:
+                if cls_name in self.class_dict:
+                    cls = self.class_dict[cls_name]
                     self.__objects[obj_id] = cls(**obj_data)
         except FileNotFoundError:
             pass
-
