a45e89bae1daf4be69acdb330825aa7875ee567e
Felipe Santos <felipecassiors@gmail.com>
2024-02-22 12:04:29 -0500

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,43 +1,51 @@
 #!/usr/bin/python3
-from uuid import uuid4
+import uuid
 from datetime import datetime
+from models import storage
 
 class BaseModel:
-    """Defines all common attributes/methods for other classes."""
-    
+    """
+    Defines all common attributes/methods for other classes.
+    """
+
     def __init__(self, *args, **kwargs):
-        """Initialization of the base model"""
+        """
+        Initializes a new instance of BaseModel.
+        """
         if kwargs:
             for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                elif key == "created_at" or key == "updated_at":
-                    value = datetime.fromisoformat(value)
-                setattr(self, key, value)
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                if key != '__class__':
+                    setattr(self, key, value)
         else:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
-        """String representation of the BaseModel class"""
+        """
+        Returns a string representation of the BaseModel instance.
+        """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
-  def save(self):
-        """Updates updated_at with the current datetime and saves to file."""
+    def save(self):
+        """
+        Updates the instance's updated_at attribute with the current datetime
+        and saves the instance to the file storage.
+        """
         self.updated_at = datetime.now()
-        storage.new(self)
         storage.save()
 
-    def __init__(self, *args, **kwargs):
-        """Initialization of the base model."""
-        # existing initialization logic...
-        if not kwargs:
-            storage.new(self)
-            
-    def to_dict(self):
-        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
-        model_dict = dict(self.__dict__)
-        model_dict["__class__"] = self.__class__.__name__
-        model_dict["created_at"] = model_dict["created_at"].isoformat()
-        model_dict["updated_at"] = model_dict["updated_at"].isoformat()
+    def to_dict(self)
+        """
+        Returns a dictionary containing all keys/values of the instance's __dict__.
+        Includes the class name under a key '__class__'.
+        Converts datetime objects to ISO format strings.
+        """
+        model_dict = self.__dict__.copy()
+        model_dict['__class__'] = self.__class__.__name__
+        model_dict['created_at'] = model_dict['created_at'].isoformat()
+        model_dict['updated_at'] = model_dict['updated_at'].isoformat()
         return model_dict
+
