28e533134271530e6c75d44bb8c557d4a4e72e51
Felipe Santos <felipecassiors@gmail.com>
2024-02-22 20:10:33 -0500

    console.py

--- console.py
+++ console.py
@@ -3,16 +3,24 @@ import cmd
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
-# Import other models here as they are created
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
     class_dict = {
         'BaseModel': BaseModel,
         'User': User,
-        # Include other classes here
+        'Place': Place,
+        'State': State,
+        'City': City,
+        'Amenity': Amenity,
+        'Review': Review,
     }
-
+    
     def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,41 +1,41 @@
 import json
 from models.base_model import BaseModel
-from models.user import User  # Import additional classes as needed
+# Importing all possible classes
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 
 class FileStorage:
-    __file_path = "file.json"  # Path to the JSON file for storing objects
-    __objects = {}  # Dictionary to store all objects by <class name>.id
+    __file_path = "file.json"
+    __objects = {}
 
-    # A dictionary mapping class names to classes, used for deserialization
-    class_dict = {
-        "BaseModel": BaseModel,
-        "User": User,
-        # Add other classes to this dictionary as you create them
-    }
     def all(self):
         """Returns the dictionary __objects."""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds an object to the __objects dictionary."""
-        key = f"{obj.__class__.__name__}.{obj.id}"
-        self.__objects[key] = obj
+        if obj:
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            FileStorage.__objects[key] = obj
 
     def save(self):
         """Serializes __objects to the JSON file (__file_path)."""
-        obj_dict = {obj_id: obj.to_dict() for obj_id, obj in self.__objects.items()}
-        with open(self.__file_path, 'w') as f:
-            json.dump(obj_dict, f)
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump({k: v.to_dict() for k, v in FileStorage.__objects.items()}, f)
 
     def reload(self):
         """Deserializes the JSON file (__file_path) to __objects if it exists."""
         try:
-            with open(self.__file_path, 'r') as f:
-                obj_dict = json.load(f)
-            for obj_id, obj_data in obj_dict.items():
+            with open(FileStorage.__file_path, 'r') as f:
+                objects = json.load(f)
+            for obj_id, obj_data in objects.items():
                 cls_name = obj_data['__class__']
-                cls = globals()[cls_name] if cls_name in globals() else None
+                cls = globals().get(cls_name)
                 if cls:
-                    self.__objects[obj_id] = cls(**obj_data)
+                    FileStorage.__objects[obj_id] = cls(**obj_data)
         except FileNotFoundError:
             pass
