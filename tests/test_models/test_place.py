78b6cc7494a8405de3c75ff0a772ce99f5ea1a46
Felipe Santos <felipecassiors@gmail.com>
2024-02-27 12:39:13 -0500

    console.py

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,39 +1,50 @@
 #!/usr/bin/python3
+'''
+    test_place.py
+    
+    Unittest for Class Place
+'''
 import unittest
 from models.place import Place
 from datetime import datetime
 
 class TestPlace(unittest.TestCase):
-    """Tests for the Place class."""
+    '''
+        Class Place unittest
+    '''
+
     def setUp(self):
         """Set up test methods."""
-        self.place_instance = Place()
-        self.place_instance.name = "My lovely house"
-        self.place_instance.city_id = "0001"
-        self.place_instance.user_id = "0001"
-        self.place_instance.number_rooms = 4
-        self.place_instance.number_bathrooms = 2
-        self.place_instance.max_guest = 2
-        self.place_instance.price_by_night = 100
-        self.place_instance.latitude = 120.0
-        self.place_instance.longitude = 90.0
-        self.place_instance.amenity_ids = []
+        self.place = Place()
+        self.place.name = "My lovely house"
+        self.place.city_id = "0001"
+        self.place.user_id = "0001"
+        self.place.number_rooms = 4
+        self.place.number_bathrooms = 2
+        self.place.max_guest = 2
+        self.place.price_by_night = 100
+        self.place.latitude = 120.0
+        self.place.longitude = 90.0
+        self.place.amenity_ids = []
 
-    def test_attributes(self):
-        """Test the attributes of Place instances."""
-        self.assertEqual(self.place_instance.name, "My lovely house")
-        self.assertEqual(self.place_instance.city_id, "0001")
-        self.assertEqual(self.place_instance.user_id, "0001")
-        self.assertEqual(self.place_instance.number_rooms, 4)
-        self.assertEqual(self.place_instance.number_bathrooms, 2)
-        self.assertEqual(self.place_instance.max_guest, 2)
-        self.assertEqual(self.place_instance.price_by_night, 100)
-        self.assertEqual(self.place_instance.latitude, 120.0)
-        self.assertEqual(self.place_instance.longitude, 90.0)
-        self.assertEqual(self.place_instance.amenity_ids, [])
-        self.assertIsInstance(self.place_instance.id, str)
-        self.assertIsInstance(self.place_instance.created_at, datetime)
-        self.assertIsInstance(self.place_instance.updated_at, datetime)
+    def test_place_attrs(self):
+        '''
+            Class Place test cases
+        '''
+        self.assertEqual(self.place.city_id, "0001")
+        self.assertEqual(self.place.user_id, "0001")
+        self.assertEqual(self.place.name, "My lovely house")
+        self.assertEqual(self.place.description, "")
+        self.assertEqual(self.place.number_rooms, 4)
+        self.assertEqual(self.place.number_bathrooms, 2)
+        self.assertEqual(self.place.max_guest, 2)
+        self.assertEqual(self.place.price_by_night, 100)
+        self.assertEqual(self.place.latitude, 120.0)
+        self.assertEqual(self.place.longitude, 90.0)
+        self.assertEqual(self.place.amenity_ids, [])
+        self.assertIsInstance(self.place.id, str)
+        self.assertIsInstance(self.place.created_at, datetime)
+        self.assertIsInstance(self.place.updated_at, datetime)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -23,3 +23,30 @@ class TestReview(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
+'''
+    test_review.py
+    
+    Unittest for class Review
+'''
+import unittest
+from models.review import Review
+
+
+class TestReview(unittest.TestCase):
+    '''
+        Class Review unittest
+    '''
+
+    def test_review_attributes(self):
+        '''
+            Class Review test cases
+        '''
+        review = Review()
+        self.assertEqual(review.place_id, "")
+        self.assertEqual(review.user_id, "")
+        self.assertEqual(review.text, "")
+
+
+if __name__ == '__main__':
+    unittest.main()
